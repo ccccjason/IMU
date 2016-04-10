ae4f74a316ce645830ea126c3d78dc479a88e276
gigi <gigi>
2016-04-10 20:19:32 +0800

    update

--- sensorservice_test/sensorTracker_test/sensorservicetest.cpp
+++ sensorservice_test/sensorTracker_test/sensorservicetest.cpp
@@ -17,17 +17,43 @@
 #include <android/sensor.h>
 #include <android/looper.h>
 #include <stdio.h>
+#include <math.h>
 
 #define ASENSOR_TYPE_ROTATION_VECTOR 11
 #define SENSOR_LOOPER_ID 20
+#define M_PI 3.1415926535897932384626433832795
+
+typedef struct  Quatdata_ {
+    double quat_w;
+    double quat_x;
+    double quat_y;
+    double quat_z;
+} Quatdata;
 
 ASensorManager* mSensorManager;
 const ASensor* mOrientationSensor;
 ASensorEventQueue* mSensorEventQueue;
 
+inline void QuaternionToEuler(double* data, Quatdata* q)
+{
+    // yaw:
+    data[0] = atan2(2 * (q->quat_x * q->quat_y + q->quat_w * q->quat_z),
+                    q->quat_w * q->quat_w + q->quat_x * q->quat_x - q->quat_y * q->quat_y -
+                    q->quat_z * q->quat_z);
+    // pitch:
+    data[1] = asin(-2 * (q->quat_x * q->quat_z - q->quat_w * q->quat_y));
+
+    // roll:
+    data[2] = atan2(2 * (q->quat_y * q->quat_z + q->quat_w * q->quat_x),
+                    q->quat_w * q->quat_w - q->quat_x * q->quat_x - q->quat_y * q->quat_y +
+                    q->quat_z * q->quat_z);
+}
+
 void updateOrientation()
 {
     ASensorEvent e;
+    double pose_ypr[3] = {0};
+    Quatdata quat_data;
 
     // If there are events, drain them and grab only the last one
     // If there aren't, then don't set anything
@@ -35,11 +61,20 @@ void updateOrientation()
         while (ASensorEventQueue_hasEvents(mSensorEventQueue) > 0) {
             ASensorEventQueue_getEvents(mSensorEventQueue, &e, 1);
             //LOGI("Sensor: %f %f %f %f", e.data[3], e.data[0], e.data[1], e.data[2]);
-            printf("Sensor: %f %f %f %f\n", e.data[3], e.data[0], e.data[1], e.data[2]);
+            quat_data.quat_w = e.data[3];
+            quat_data.quat_x = e.data[2];
+            quat_data.quat_y = e.data[1];
+            quat_data.quat_z = e.data[0];
+            QuaternionToEuler(pose_ypr, &quat_data);
+
+            printf("pose_yaw=%f, pose_pitch=%f, pose_roll=%f\n",
+                   (pose_ypr[0] * 180) / M_PI,
+                   (pose_ypr[1] * 180) / M_PI,
+                   (pose_ypr[2] * 180) / M_PI);
+
+            // printf("Sensor: %f %f %f %f\n", e.data[3], e.data[0], e.data[1], e.data[2]);
         }
 
-        // mQuat = Quat(e.data[3], e.data[0], e.data[1], e.data[2]);
-        //LOGI("Done, setting.");
     }
 }
 
